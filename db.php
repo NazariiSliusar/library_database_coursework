c0655d17ffcabbc803034420554802fd1c8f84c8
Nazarii <nazrii.sliusar@gmail.com>
2025-11-13 23:07:06 +0100

    First commit

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "1106";
+$dbname = "library";
+$port = 3307;
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}echo "Connected successfully";
+?>
